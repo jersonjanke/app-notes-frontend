6440fc8eff74cd92639c2bca31632386afc38b77
Jerson Janke <jerson.janke@saudeid.com.br>
2022-06-21 18:04:46 -0300

    feat test config minify

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   compiler: {
-    swcMinify: true,
     styledComponents: true,
+    removeConsole: true,
+    removeConsole: {
+      exclude: ['error'],
+    },
   },
 };
 
