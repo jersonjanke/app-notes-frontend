281f8adc0fbca659c7575d2e51ebcfdd0cd029b0
Jerson Janke <jerson.janke@saudeid.com.br>
2022-08-22 16:29:57 -0300

    fix theme toast

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -3,6 +3,6 @@ import { toast } from 'react-toastify';
 export const toastMSG = (msg: string, type: 'success' | 'error') => {
   toast(msg, {
     type,
-    theme: 'colored',
+    theme: 'dark',
   });
 };
