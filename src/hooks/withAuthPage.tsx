8a7437030824d2fb5a5b1d1fd8ad629c5819a92e
Jerson Janke <jerson.janke@saudeid.com.br>
2022-08-29 12:33:47 -0300

    fix cache

--- src/hooks/withAuthPage.tsx
+++ src/hooks/withAuthPage.tsx
@@ -3,13 +3,20 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { pages } from 'utils/pages';
 import { cookies, keys } from 'utils/cookies';
+import { useDispatch } from 'react-redux';
+import { userUpdate } from 'store/actions/user';
 
 export default function withAuthPage(PageComponent: React.FC) {
   const WrappedPageComponent = () => {
     const router = useRouter();
     const token = cookies.get(keys.user);
+    const dispatch = useDispatch();
 
     useEffect(() => {
+      const payload = JSON.parse(cookies.get(keys.user));
+      axios.defaults.headers.common['Authorization'] = payload.token;
+      dispatch(userUpdate(payload));
+
       if (!token) {
         cookies.remove(keys.user);
         router.push(pages.root);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,11 @@
-import axios from 'axios';
 import Login from 'forms/Login/Login';
 import type { NextPage } from 'next';
 import { Row, Col, Hidden } from 'react-grid-system';
 import Image from 'next/image';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import { StoreData } from 'types/Login';
-import { userUpdate } from 'store/actions/user';
+
 import Head from 'next/head';
-import { cookies, keys } from 'utils/cookies';
 
 const Home: NextPage = () => {
-  const dispatch = useDispatch();
-  const [data, setData] = useState();
-  const user = useSelector((state: StoreData) => state.user);
-
-  useEffect(() => {
-    cookies.remove(keys.user);
-  }, []);
-
-  useEffect(() => {
-    const userData = cookies.get(keys.user);
-    userData && setData(JSON.parse(userData as string));
-    if (user?.token?.length) {
-      axios.defaults.headers.common['Authorization'] = user.token;
-    }
-  }, [user]);
-
-  useEffect(() => {
-    if (data && !user?.token?.length) {
-      dispatch(userUpdate(data));
-    }
-  }, [data, dispatch, user]);
-
   return (
     <>
       <Head>
