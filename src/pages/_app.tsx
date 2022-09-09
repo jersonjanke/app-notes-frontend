4dbb27fec6a9ae468ce0c1240155e8752443d706
Jerson Janke <jerson.janke@saudeid.com.br>
2022-09-09 12:24:22 -0300

    add script facebook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,6 +37,27 @@ function MyApp({ Component, pageProps }: AppProps) {
           gtag('config', '${ID}');
         `}
         </Script>
+
+        <Script id="facebook-login" strategy="afterInteractive">
+          {`
+            window.fbAsyncInit = function() {
+              FB.init({
+                appId      : '5542817972449076',
+                xfbml      : true,
+                version    : 'v14.0'
+              });
+              FB.AppEvents.logPageView();
+            };
+
+            (function(d, s, id){
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) {return;}
+              js = d.createElement(s); js.id = id;
+              js.src = "https://connect.facebook.net/en_US/sdk.js";
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        `}
+        </Script>
       </>
     </>
   );
